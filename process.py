70cf38af23f65e54fcf5dc3255f64c053906aa38
xwtfd <xwtfd@outlook.com>
2019-04-12 01:15:39 +0800

    code reform

--- process.py
+++ process.py
@@ -4,7 +4,7 @@ from keras.models import Sequential
 from data_preprocessing import my_DataGenerator
 
 # Parameters
-params = {'dim': (32,32,32),
+params = {'dim': (32, 32, 32),
           'batch_size': 64,
           'n_classes': 2,
           'n_channels': 1,
@@ -13,7 +13,7 @@ params = {'dim': (32,32,32),
 # Datasets
 data = np.load('data.npy')
 data_list = data['item']
-labels = data['label']# Labels
+labels = data['label']  # Labels
 # random
 index = np.random.permutation(len(labels))
 random_data = data[index]
@@ -21,8 +21,8 @@ random_labels = labels[index]
 # train and validation
 train_size = 25000
 partition = {}
-partition['train'] = [x for x in range(train_size)]# IDs of train
-partition['validation'] = [x for x in range(train_size,len(index))]# IDs of validation
+partition['train'] = [x for x in range(train_size)]  # IDs of train
+partition['validation'] = [x for x in range(train_size, len(index))]  # IDs of validation
 
 # Generators
 training_generator = my_DataGenerator(partition['train'], random_data, random_labels, **params)
@@ -30,7 +30,7 @@ validation_generator = my_DataGenerator(partition['validation'], random_data, la
 
 # Design model
 model = Sequential()
-[...] # Architecture
+[...]  # Architecture
 model.compile()
 
 # Train model on dataset
